81773fcddf5a3da6c476033b6582f2bbc99a4a61
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-17 20:04:00 -0700

    Changed structure of melons.py dictionaries into one cohesive dictionary

--- melons.py
+++ melons.py
@@ -1,23 +1,33 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+from multiprocessing.sharedctypes import Value
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+melon = {
+    'Honeydew': {'price': 0.99,
+    'seedlessness': True,
+    'flesh_color': None,
+    'rind_color': None,
+    'average_weight': None},
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+    'Crenshaw': {'price': 2.00,
+    'seedlessness': False,
+    'flesh_color': None,
+    'rind_color': None,
+    'average_weight': None},
+
+    'Crane': {'price': 2.50,
+    'seedlessness': False,
+    'flesh_color': None,
+    'rind_color': None,
+    'average_weight': None},
+
+    'Casaba': {'price': 2.50,
+    'seedlessness': False,
+    'flesh_color': None,
+    'rind_color': None,
+    'average_weight': None},
+
+    'Cantaloupe': {'price': 0.99,
+    'seedlessness': False,
+    'flesh_color': None,
+    'rind_color': None,
+    'average_weight': None}
 }
