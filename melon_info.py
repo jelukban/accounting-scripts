dd11df118ca5fc41ffa8415695b20983a4cc422f
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-17 20:09:21 -0700

    Updated melon_info.py to take in the newly formatted melons.py

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,17 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melon
 
 
-def print_melon(name, price, seedless):
+def print_melon():
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for name, value_dict in melon.items():
+        print(name.upper())
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+        for attribute, value in melon[name].items():
+            print(f"{attribute} : {value}")
+        print("--------------------------------------")
 
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+print(print_melon())
